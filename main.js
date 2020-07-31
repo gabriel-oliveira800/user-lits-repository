d01cecf412d8c4a1666c2aea4fba58538f024855
gabrielscorehero <gabrielscorehero55@gmail.com>
2020-07-30 22:35:31 -0300

    created list user repositorys

---
+++ main.js
@@ -0,0 +1,45 @@
+var listElement = document.querySelector('#app ul');
+
+var inputElement = document.querySelector('#app input');
+var btnEmlement = document.querySelector('#app button');
+
+var repository = [];
+
+function renderElement() {
+    listElement.innerHTML = '';
+
+    for (repo of repository) {
+        var itemList = document.createElement('li');
+        var textItemList = document.createTextNode(repo);
+
+        itemList.appendChild(textItemList);
+        listElement.appendChild(itemList);
+    }
+}
+
+function getUserRepo(userName) {
+    repository = [];
+    axios.get(`https://api.github.com/users/${userName}/repos`).then((result) => {
+        for (repo of result['data']) {
+            const { name } = repo;
+            repository.push(name);
+        }
+
+        renderElement();
+
+    }).catch((err) => console.log(err));
+}
+
+function searchUser() {
+    const textValue = inputElement.value;
+    if (!textValue) {
+        alert('The name cannot be empty');
+        return;
+    }
+
+    getUserRepo(textValue);
+    inputElement.value = '';
+}
+
+btnEmlement.onclick = searchUser;
+renderElement();
